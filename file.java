8803f41f26a055a1ce51b518bba5d3577b015b06
Higgs32584 <mjhiggins2@loyola.edu>
2022-03-14 21:40:52 -0400

    draft

---
+++ file.java
@@ -0,0 +1,21 @@
+public class BinaryTree{
+    // The tree should include methods to insert integers and to print the tree inorder, preorder, and postorder. The tree need not be balanced.
+    Node head;
+    public class Node{
+        int num;
+        Node left;
+        Node right;
+    }
+    public void inOrder(){
+        
+    }
+    public void preOrder(){
+        
+    }
+    public void postOrder(){
+        
+    }
+    public static void main(String[] args){
+        System.out.println("yo");
+    }
+}
